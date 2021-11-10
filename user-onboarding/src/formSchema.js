b0b0ae9e0e408ec9ae8e2524d436e1422602c016
Danielle Strazzeri <dstrazzeri303@gmail.com"git config --global user.name "Danielle Strazzeri>
2021-11-10 15:38:56 -0500

    fixed bug in formSchema

--- user-onboarding/src/formSchema.js
+++ user-onboarding/src/formSchema.js
@@ -1,24 +1,24 @@
 import * as yup from 'yup';
 
 const formSchema = yup.object().shape({
-    // first_name: yup
-    //   .string()
-    //   .trim()
-    //   .required('First name is required!')
-    //   .min(1, 'First name must be 1 or more characters long, silly'),
-    //   last_name: yup
-    //   .string()
-    //   .trim()
-    //   .required('Last Name is required!')
-    //   .min(1, 'Last name must be 1 or more characters long, silly'),
-    // email: yup
-    //   .string()
-    //   .email('Must be a valid email address!')
-    //   .required('Email is required!'),
-    // password: yup
-    //   .string()
-    //   .required('Must be a valid password!'),
-    // tos: yup.boolean().oneOf(true)
+    first_name: yup
+      .string()
+      .trim()
+      .required('First name is required!')
+      .min(1, 'First name must be 1 or more characters long, silly'),
+      last_name: yup
+      .string()
+      .trim()
+      .required('Last Name is required!')
+      .min(1, 'Last name must be 1 or more characters long, silly'),
+    email: yup
+      .string()
+      .email('Must be a valid email address!')
+      .required('Email is required!'),
+    password: yup
+      .string()
+      .required('Must be a valid password!'),
+    tos: yup.boolean(false),
 });
 
 export default formSchema
